30e87321326e3490b2aabdc5afec0add9fc79c53
liangyong <liangyong@xiaomi.com>
2015-11-24 09:39:00 +0800

    create project

---
+++ src/main/java/com/liyo/WholeWorld/WholeWorldMain.java
@@ -0,0 +1,9 @@
+package com.liyo.WholeWorld;
+/**
+ * Created by liyo on 15-11-24.
+ */
+public class WholeWorldMain {
+    public static void main(String[] args) {
+        System.out.println("Hello World.");
+    }
+}
